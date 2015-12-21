358c6b8cdfc2d0afc0acc7d40505ea774ea6160c
Xavier G <contact@xavier-guichet.fr>
2015-12-21 15:32:41 +0100

    Import et envoie de masse

---
+++ src/AppBundle/Controller/ImportController.php
@@ -0,0 +1,190 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+
+use Goodby\CSV\Import\Standard\Interpreter;
+use Goodby\CSV\Import\Standard\Lexer;
+use Goodby\CSV\Import\Standard\LexerConfig;
+
+use AppBundle\Entity\VoeuxPropose;
+use AppBundle\Entity\Questionnaire;
+use AppBundle\Entity\ContenuMail;
+use XG\PeopleBundle\Entity\People;
+
+class ImportController extends Controller
+{
+    private $listQuestionnaire;
+    private $listContenu;
+
+    /**
+     * @Route("/import/upload", name="app_import_csv_file_upload")
+     */
+    public function uploadAction()
+    {
+      $form = $this->getImportForm();
+
+      return $this->render('AppBundle:Import:upload.html.twig', array(
+          'form' => $form->createView()
+          ));
+    }
+
+    /**
+     * @Route("/import/verification", name="app_import_csv_file_upload")
+     */
+    public function check_validityAction(Request $request) {
+      $table = array();
+      $em = $this->getDoctrine()->getManager();
+      $repositoryQuestionnaire = $em->getRepository('AppBundle\Entity\Questionnaire');
+      $repositoryContenuMail = $em->getRepository('AppBundle\Entity\ContenuMail');
+  		$this->listQuestionnaire = $repositoryQuestionnaire->findAll();
+  		$this->listContenu = $repositoryContenuMail->findAll();
+
+      $validator = $this->get('validator');
+
+      $form = $this->getImportForm();
+
+      $form->handleRequest($request);
+      if ($form->isValid()) {
+        $file = $form->get('importedcsv')->getData();
+        $fileName = md5(uniqid()).'.'.$file->guessExtension();
+        $uploadDir = $this->container->getParameter('kernel.root_dir').'/../web/uploads/csv/';
+        $file->move($uploadDir, $fileName);
+        $filepath = $uploadDir.$fileName;
+        $phpExcelObject = $this->get('phpexcel')->createPHPExcelObject();
+        $objPHPExcel = \PHPExcel_IOFactory::load($filepath);
+
+        $row_count = 0;
+        $error_count = 0;
+        foreach ($objPHPExcel->getWorksheetIterator() as $worksheet) {
+          foreach ($worksheet->getRowIterator() as $row) {
+            $row_count++;
+            $cellIterator = $row->getCellIterator();
+            $cellIterator->setIterateOnlyExistingCells(false);
+            $VoeuxPropose = new VoeuxPropose();
+            $VoeuxPropose = $this->CsvRowToVoeuxObject($cellIterator);
+            if($VoeuxPropose !== false) {
+              if($VoeuxPropose !== "Bad cell count") {
+                $errors = $validator->validate($VoeuxPropose);
+                if (count($errors) > 0) {
+                  $error_count++;
+                  $table[$row_count]["errors"] = $errors;
+                  $table[$row_count]["VoeuxPropose"] = $VoeuxPropose;
+                }
+                elseif(!preg_match('/@(freetouch\.fr|visibleo\.fr|koba\.com){1}$/',$VoeuxPropose->getEnvoyeurEmail())) {
+                  $error_count++;
+                  $table[$row_count]["errors"][]["message"] = "L'adresse mail de l'expéditeur n'est pas autorisé";
+                  $table[$row_count]["VoeuxPropose"] = $VoeuxPropose;
+                }
+                else {
+                  $table[$row_count]["VoeuxPropose"] = $VoeuxPropose;
+                }
+              }
+              else {
+                $error_count++;
+                $table[$row_count]["errors"][]["message"] = "Cette ligne contient un ou plusieurs champs vide. Elle n'a pas pu être gérée.";
+              }
+            }
+          }
+        }
+
+        if ($form->get('check_and_send')->isClicked() && $error_count == 0) {
+          foreach($table as $row) {
+            $curVoeuxPropose = $row["VoeuxPropose"];
+            $tokenmail = md5(uniqid(null, true).date("YmdHis"));
+            $curVoeuxPropose->setTokenmail($tokenmail);
+            $em->persist($curVoeuxPropose);
+
+            //Preparation contenu mail
+            if($curVoeuxPropose->getPeople()->getIsmale()) {
+                $civilité = "Cher ".$curVoeuxPropose->getPeople()->getPrenom();
+            }
+            else {
+                $civilité = "Chère ".$curVoeuxPropose->getPeople()->getPrenom();
+            }
+            $link = $this->container->get('router')->generate('app_front_form', array('tokenmail' => $tokenmail), true);
+            $mailcontent = array('link' => $link,
+                                 'token' => $tokenmail,
+                                 'civilite' => $civilité,
+                                 'mailtexte' => $curVoeuxPropose->getContenuMail()->getContenuTxt()
+                                 );
+
+               if(preg_match('/@(freetouch\.fr|visibleo\.fr|koba\.com){1}$/',$curVoeuxPropose->getEnvoyeurEmail())) {
+                   $to = $curVoeuxPropose->getPeople()->getEmail();
+                   if (!$this->get('mail_to_user')->sendBestWishesEmail($to,$mailcontent,$curVoeuxPropose->getEnvoyeurEmail())) {
+                       throw $this->createNotFoundException('Unable to send Best Wishes mail.');
+                   }
+               }
+          }
+          $em->flush();
+        }
+      }
+
+      return $this->render('AppBundle:Import:checkvalidity.html.twig', array(
+        'form' => $form->createView(),
+        'table_check' => $table
+      ));
+    }
+
+    /**
+     * Retourne le formulaire d'upload de fichiers
+     */
+    private function getImportForm() {
+      return $this->createFormBuilder()
+                   ->setAction($this->generateUrl('app_import_csv_check_validity'))
+                   ->add('importedcsv', FileType::class, array('label' => 'Votre fichier'))
+                   ->add('check', 'submit', array('label' => 'Vérifier'))
+                   ->add('check_and_send', 'submit', array('label' => 'Vérifier et envoyer les voeux'))
+                   ->getForm();
+    }
+
+    /**
+     * Converti une ligne de cellule en objet VoeuxPropose
+     */
+    private function CsvRowToVoeuxObject($cellIterator) {
+      $em = $this->getDoctrine()->getManager();
+      $row = array();
+      foreach ($cellIterator as $cell) {
+              if (!is_null($cell) && !is_null($cell->getCalculatedValue())) {
+                if(is_numeric($cell->getCalculatedValue())) {
+                  $row[] = (int) $cell->getCalculatedValue();
+                }
+                else {
+                $row[] = trim($cell->getCalculatedValue());
+                }
+              }
+      }
+      if($row[0] == "Raison Sociale") { return false;}
+      if(count($row) != 11) { return "Bad cell count";}
+
+      $VoeuxPropose = new VoeuxPropose();
+      $Questionnaire = new Questionnaire();
+      $People = new People();
+      $ContenuMail = new ContenuMail();
+
+      $People->setSociete($row[0]);
+      $People->setAdresse($row[1]);
+      $People->setCodepostal($row[2]);
+      $People->setVille($row[3]);
+      $People->setNom($row[4]);
+      $People->setPrenom($row[5]);
+      $People->setEmail($row[6]);
+      $People->setIsmale($row[7]);
+
+      $em->persist($People);
+      $contenumail = $this->listContenu[$row[8] - 1];
+      $questionnaire = $this->listQuestionnaire[$row[9] - 1];
+
+      $VoeuxPropose->setQuestionnaire($questionnaire);
+      $VoeuxPropose->setPeople($People);
+      $VoeuxPropose->setContenuMail($contenumail);
+      $VoeuxPropose->setEnvoyeurEmail($row[10]);
+      $em->persist($VoeuxPropose);
+      return $VoeuxPropose;
+    }
+}
